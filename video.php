9d654cf68a88c15fa7fa410dc621858ad7b30000
dreyky <50557509+dreyky@users.noreply.github.com>
2021-01-01 18:18:20 -0600

    Create video.php

---
+++ video.php
@@ -0,0 +1,44 @@
+
+
+
+
+<?php
+
+
+
+
+
+
+
+
+error_reporting(0);
+$get_url = $_GET["url"];
+if (is_null($get_url))
+{
+echo "Enter Youtube Link";
+}
+else
+{
+// youtube-dl -f best -g IS AUTO SELECTED
+$youtube_downloader = "youtube-dl ".$get_url." 2>&1";
+
+
+
+$stream = exec($youtube_downloader);
+
+
+
+ echo $stream;
+ 
+
+// echo readfile($stream);
+// echo readgzfile($stream);
+
+
+
+}
+
+
+
+
+?>
